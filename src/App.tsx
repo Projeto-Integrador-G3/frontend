36bca0d88374db68f7e0f9673adeb6437f2ba699
Antonio Bandeira <104794351+antonioviniciusvb@users.noreply.github.com>
2024-01-24 12:31:42 -0300

    :sparkles: feat: Atualização do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login';
 import Cadastro from './pages/cadastro/Cadastro';
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 
 
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
         <Navbar />
 
@@ -32,6 +34,7 @@ function App() {
         </div>
         <Footer />
       </BrowserRouter>
+      </AuthProvider>
 
     </>
   )

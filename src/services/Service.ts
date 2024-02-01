6cd90b42aef8287b39b70ebc29755a1cb7034d30
Elisabeth Aparecida <beth_ap@live.com>
2024-02-01 11:32:00 -0300

    alteração do formato do arquivo

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -12,8 +12,8 @@ export const login  = async(url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
 }
-export const buscar = async(url: string, setDados: Function, header: Object) => {
-    const resposta = await api.get(url, header)
+export const buscar = async(url: string, setDados: Function) => {
+    const resposta = await api.get(url)
     setDados(resposta.data)
   }
   export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
@@ -26,5 +26,4 @@ export const buscar = async(url: string, setDados: Function, header: Object) => 
   }
   export const deletar = async(url: string, header: Object) => {
     await api.delete(url, header)
-    
   }

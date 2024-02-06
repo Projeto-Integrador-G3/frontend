36c80f33bdd702701469b3019a1470875ef828e1
Kendal Katherine <kendal.katherine3@gmail.com>
2024-02-06 15:00:18 -0300

    Responsividade card produtos

--- src/components/produtos/deletarProduto/DeletarProduto.tsx
+++ src/components/produtos/deletarProduto/DeletarProduto.tsx
@@ -72,20 +72,24 @@ function DeletarProduto() {
   }
 
   return (
-    <div className='container w-1/3 mx-auto'>
-      <h1 className='text-4xl text-center my-4'>Deletar produto</h1>
+    <div className='container md:w-1/3 mx-auto p-2 md:p-0 h-[80vh] md:my-auto'>
+      <h1 className='text-3xl md:text-4xl text-center my-4'>Deletar produto</h1>
 
       <p className='text-center font-semibold mb-4'>Você tem certeza de que deseja apagar a produto a seguir?</p>
 
       <div className='border flex flex-col rounded-2xl overflow-hidden justify-between'>
         <header className='py-2 px-6 bg-green-400 text-white font-bold text-2xl'>Produto</header>
         <div className="p-4">
-          <p className='text-xl h-full'>{produto.nome}</p>
-          <p>{produto.preco}</p>
+          
+          <p className='font-bold text-xl h-full'>{produto.nome}</p>
+          <p>{`Código: ${produto.id}`}</p>
+          <p>{produto.descricao}</p>
+          <p>{produto.cor}</p>
+          <p>{produto.categoria?.descricao}</p>
         </div>
-        <div className="flex">
-          <button className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2' onClick={retornar} >Não</button>
-          <button className='w-full text-slate-100 bg-green-300 hover:bg-green-400 flex items-center justify-center' onClick={deletarProduto} >
+        <div className="flex flex-col md:flex-row">
+          <button className='text-slate-100 bg-red-400 hover:bg-red-700 w-full py-2' onClick={retornar} >Não</button>
+          <button className='w-full text-white bg-green-300 hover:bg-green-400 font-bold  flex items-center justify-center py-2' onClick={deletarProduto} >
             {isLoading ? <RotatingLines
               strokeColor="white"
               strokeWidth="5"

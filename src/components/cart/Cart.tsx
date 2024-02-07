fe7b1404e71f434b1413bc058e40225e751ea95b
Kendal Katherine <kendal.katherine3@gmail.com>
2024-02-06 22:45:11 -0300

    Responsividade Carrinho de Compras

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -4,43 +4,31 @@ import { CartContext } from "../../contexts/CartContext";
 import CardCart from "../cardCart/CardCart";
 
 function Cart() {
+  const navigate = useNavigate();
+  const { items, limparCart } = useContext(CartContext);
 
-    const navigate = useNavigate();
-    
-    const { items, limparCart } = useContext(CartContext)
-    
-    return (
-        <div className="
-                flex 
-                flex-col
-                justify-center
-                h-[80vh]
-                ">
+  return (
+    <div className="flex flex-col justify-center min-h-screen p-4 md:p-8">
+      <h1 className="text-4xl text-center my-4">Carrinho de Compras</h1>
+      <h2 className="text-2xl text-center my-4">
+        {items.length === 0 ? "O Carrinho está vazio!" : ""}
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+        {items.map((produto) => (
+          <CardCart key={produto.id} item={produto} />
+        ))}
+      </div>
 
-            <h1 className="text-4xl text-center my-4">
-                Carrinho de Compras
-            </h1>
-            <h2 className="text-2xl text-center my-4">
-                { items.length === 0 ? 'O Carrinho está vazio!' : ''}
-            </h2>
-            <div className='container mx-auto my-4 grid grid-cols-1 
-                            md:grid-cols-2 lg:grid-cols-5 gap-4'>
-                {
-                    items.map(produto => (
-                        <CardCart key={produto.id} item={produto} />
-                    ))
-                }
-            </div>
-
-            <button className="rounded text-white bg-green-400 
-        hover:bg-green-300 px-4 py-2 mx-auto flex justify-center cursor-pointer"
-                type="submit" 
-                disabled={items.length === 0 ? true : false}
-                onClick={limparCart}>
-                Finalizar Compra
-            </button>
-        </div>
-    )
+      <button
+        className="rounded text-white bg-green-400 hover:bg-green-300 px-4 py-2 mx-auto flex justify-center cursor-pointer"
+        type="submit"
+        disabled={items.length === 0 ? true : false}
+        onClick={limparCart}
+      >
+        Finalizar Compra
+      </button>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
